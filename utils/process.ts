5a07f956e7f1b3553f460609141fc820ba127935
Sup3rga <wilkysynd@gmail.com>
2026-07-28 00:40:38 -0400

    add sudo for docker command combined

--- utils/process.ts
+++ utils/process.ts
@@ -119,8 +119,7 @@ export const Process = {
         },
         async runDockerCompose(){
             const cwd = process.cwd();
-            await cli.exec(`cd ${Process.config.sitePath}`);
-            await cli.exec(`sudo docker compose up -d`);
+            await cli.exec(`cd ${Process.config.sitePath} && sudo docker compose up -d`);
             if (Process.options.verbose) {
                 console.log(chalk.dim(`  ✅ docker compose exécuté !`));
             }
